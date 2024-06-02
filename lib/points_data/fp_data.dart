159f5eb017e812b0140f4d3d0245f25380bb90be
Mikolaj_Mezyk <mikolaj.mezyk@wp.pl>
2024-06-02 22:41:58 +0200

    funny point data

---
+++ lib/points_data/fp_data.dart
@@ -0,0 +1,12 @@
+import 'package:crazy_places/classes/points/funny_point.dart';
+
+List<FunnyPoint> listaFP = [
+  FunnyPoint(
+    lat: 53.197744,
+    lang: 18.373484,
+    pathLogo: "assets/icons8-house-96.png",
+    name: "Paweł Jumper",
+    description: "To jest to miejsce gdzie Paweł Jumper skakał. Hasło 'to już się kameruje!' przeszło do historii polskiego YouTuba",
+    pathYT: "https://www.youtube.com/watch?v=9cX17CeYKt0&ab_channel=Dendi321",
+  ),
+];
