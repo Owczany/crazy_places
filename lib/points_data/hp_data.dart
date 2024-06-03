695f3b79857c9743199563d4e694857d756bdfd1
Mikolaj_Mezyk <mikolaj.mezyk@wp.pl>
2024-06-03 15:53:45 +0200

    kosmetyczna zmiana punktu

--- lib/points_data/hp_data.dart
+++ lib/points_data/hp_data.dart
@@ -8,7 +8,7 @@ List<HistoricalPoint> historicalPoints = [
     icon: Icon(
       Icons.mosque_rounded,
       color: Colors.red,
-      size: 20,
+      size: 50,
     ),
     name: "Krzywa Wieża w Pizie",
     description: "Krzywa Wieża w Pizie, znana również jako La Torre Pendente di Pisa, to charakterystyczna dzwonnica katedry w Pizie, we Włoszech. Zbudowana między XII a XIV wiekiem, wieża słynie z wyraźnego nachylenia spowodowanego osiadaniem gruntu. Ma wysokość około 56 metrów i liczy osiem kondygnacji, w tym dzwonnicę na szczycie. Krzywa Wieża w Pizie jest jednym z najbardziej rozpoznawalnych zabytków na świecie i stanowi popularną atrakcję turystyczną.",
@@ -20,7 +20,7 @@ List<HistoricalPoint> historicalPoints = [
     icon: Icon(
       Icons.mosque_rounded,
       color: Colors.red,
-      size: 20,
+      size: 50,
     ),
     name: "Wieża Eiffla",
     description: "Wieża Eiffla, znana również jako La Tour Eiffel, to ikoniczna żelazna wieża znajdująca się w Paryżu, we Francji. Zbudowana w latach 1887-1889 przez Gustave'a Eiffela z okazji Wystawy Światowej, wieża osiąga wysokość 324 metrów, co czyni ją jednym z najwyższych budynków w Paryżu. Konstrukcja składa się z trzech poziomów dostępnych dla zwiedzających, z restauracjami i platformami widokowymi oferującymi spektakularne widoki na miasto. Wieża Eiffla jest jednym z najbardziej rozpoznawalnych zabytków na świecie i stanowi symbol Paryża oraz Francji.",
